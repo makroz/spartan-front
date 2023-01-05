54883aba30b9a3173dbd7440e6ae5a156ced254d
Mario Guzman PC <makroz@hotmail.com>
2023-01-05 01:10:16 -0400

    Country,state and cities table

---
+++ pages/countries.tsx
@@ -0,0 +1,67 @@
+import { Card } from "flowbite-react";
+import { useEffect, useState } from "react";
+import DataTable from "../src/components/DataTable";
+import Spinner from "../src/components/layouts/Spinner";
+import useAxios from "../src/hooks/useAxios";
+
+const countriesPage = () => {
+  const [params, setParams] = useState({
+    page: 1,
+    perPage: 10,
+    sortBy: "id",
+    orderBy: "asc",
+    searchBy: "",
+  });
+  const {
+    data: countries,
+    loaded,
+    reLoad,
+  } = useAxios("/countries", "GET", { ...params, origen: "useAxios" });
+
+  useEffect(() => {
+    reLoad({ ...params, origen: "reLoad" }, true);
+  }, [params]);
+
+  const onChangePage = (page) => {
+    if (params.page == page) return;
+    setParams({ ...params, page });
+  };
+  const onChangePerPage = (perPage) => {
+    if (params.perPage == perPage) return;
+    setParams({ ...params, perPage });
+  };
+
+  const columns = {
+    name: {
+      header: "Country",
+      className: "",
+    },
+    code: {
+      header: "Code",
+      className: "",
+    },
+  };
+
+  //if (!users) return null;
+
+  return (
+    <>
+      <h1>Countries List</h1>
+      <Card className="relative">
+        {!loaded && <Spinner />}
+        {loaded && (
+          <DataTable
+            datas={countries.data}
+            columns={columns}
+            params={{ ...params, total: countries.total }}
+            onChangePage={onChangePage}
+            onChangePerPage={onChangePerPage}
+          />
+        )}
+      </Card>
+    </>
+  );
+};
+
+export default countriesPage;
+countriesPage.auth = true;
