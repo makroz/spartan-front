367eb2bb58fa51d1a4c0c2fa47793ff7febefd31
Mario Work FOS <trabajo.makroz@gmail.com>
2023-01-06 17:30:37 -0400

    city y state

--- pages/cities.tsx
+++ pages/cities.tsx
@@ -1,7 +1,10 @@
+import { Spinner } from "flowbite-react";
 import DataCrud from "../src/components/DataCrud";
+import useAxios from "../src/hooks/useAxios";
 import { getFields } from "../src/utils/dbTools";
 
 const citiesPage = () => {
+  const { data, loaded }: any = useAxios("/states", "GET", { perPage: 0 });
   const columns = {
     name: {
       header: "Cities",
@@ -14,7 +17,13 @@ const citiesPage = () => {
     },
   };
 
-  const fields = getFields(["id", "name", "code", "status"]);
+  if (!loaded) return <Spinner />;
+  const fields = getFields(["id", "name", "state_id", "status"]);
+  fields["state_id"].inputType = "select";
+  fields["state_id"].options = data.data.map((item) => ({
+    value: item.id,
+    label: item.name + " (" + item.abv + ")",
+  }));
 
   return (
     <>
